79f37c43d5d8ee16351f1457ba5bdc6e3ad5efb3
coldshineb <shouhubyr@qq.com>
2024-02-20 01:33:47 -0500

    add lollipop_column_entrance

---
+++ src/main/java/city/gennokioku/plus/block/MetroStationBlock/LollipopColumnEntranceBlock.java
@@ -0,0 +1,85 @@
+
+package city.gennokioku.plus.block.MetroStationBlock;
+
+import city.gennokioku.plus.GennokiokuPlusElements;
+import city.gennokioku.plus.itemgroup.MetroStationBlockItemGroup;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.IBlockReader;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.registries.ObjectHolder;
+
+import java.util.Collections;
+import java.util.List;
+
+@GennokiokuPlusElements.ModElement.Tag
+public class LollipopColumnEntranceBlock extends GennokiokuPlusElements.ModElement {
+	@ObjectHolder("gennokiokuplus:lollipop_column_entrance")
+	public static final Block block = null;
+
+	public LollipopColumnEntranceBlock(GennokiokuPlusElements instance) {
+		super(instance, 121);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(
+				() -> new BlockItem(block, new Item.Properties().group(MetroStationBlockItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+	}
+
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).notSolid()
+					.setOpaque((bs, br, bp) -> false));
+			setRegistryName("lollipop_column_entrance");
+		}
+
+		@Override
+		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+			return true;
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 0;
+		}
+
+		@Override
+		public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
+			Vector3d offset = state.getOffset(world, pos);
+			return VoxelShapes.or(makeCuboidShape(6, 0, 6, 10, 16, 10))
+
+					.withOffset(offset.x, offset.y, offset.z);
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
